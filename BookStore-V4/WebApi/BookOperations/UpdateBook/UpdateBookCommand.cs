b05addb05f944c4c87aea77b37a9d056d74b8b7b
Seren Kaya <srnky62@gmail.com>
2023-02-03 14:49:25 +0300

    Add files via upload

---
+++ BookStore-V4/WebApi/BookOperations/UpdateBook/UpdateBookCommand.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using WebApi.Common;
+using WebApi.DBOperations;
+
+namespace WebApi.BookOperations.UpdateBook
+{
+    public class UpdateBookCommand
+    {
+        private readonly BookStoreDBContext _dbContext;
+        public int BookId { get; set; }
+        public UpdateBookModel Model { get; set; }
+        public UpdateBookCommand(BookStoreDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void Handle()
+        {
+            var book = _dbContext.Books.SingleOrDefault(x => x.Id == BookId);
+            if (book is null)
+                throw new InvalidOperationException("Kitap yok");
+
+            book.GenreID = Model.GenreId != default ? Model.GenreId : book.GenreID;
+            book.PageCount = Model.PageCount != default ? Model.PageCount : book.PageCount;
+            book.PublishDate = Model.PublishDate != default ? Model.PublishDate : book.PublishDate;
+            book.Title = Model.Title != default ? Model.Title : book.Title;
+
+            _dbContext.SaveChanges();
+        }
+    }
+    public class UpdateBookModel
+    {
+        public string Title { get; set; }
+        public int GenreId { get; set; }
+
+        public int PageCount { get; set; }
+
+        public DateTime PublishDate { get; set; }
+
+    }
+
+}
